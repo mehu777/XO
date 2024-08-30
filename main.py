e7d3598b31738ffe18b7984bb05c12df70c64705
mahran <mahranhorani@gmail.com>
2024-08-30 12:32:56 +0300

    divided func added

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ def print_hi(name):
 def multi(a,b):
     res=a * b
     print(res)
-
-
+def divide(c,d):
+    res=c/d
+    print(res)
 if __name__ == '__main__':
     print_hi('PyCharm')
 
 multi(3,4)
+divide(10,2)
